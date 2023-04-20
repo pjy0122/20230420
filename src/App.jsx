32a8b051887415793d5c3700319ac12fc4e5003b
pjy0122 <pzw0122@gmail.com>
2023-04-20 17:26:25 +0900

    call

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Web3 from "web3";
-import { CONTRACT_ABI, CONTRACT_ADDRESS } from "./web3.config";
+import {
+  CONTRACT_ABI,
+  CONTRACT_ADDRESS,
+  NFT_ABI,
+  NFT_ADDRESS,
+} from "./web3.config";
 
-const web3 = new Web3("https://rpc-mumbai.maticvigil.com"); // 이 위치에 작성해도 문제가 없기 때문에 여기 쓰는것.
+const web3 = new Web3(window.ethereum);
 const contract = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
+const nftContract = new web3.eth.Contract(NFT_ABI, NFT_ADDRESS);
 
 function App() {
-  const [account, setAccount] = useState(""); // 함수형 컴포넌트 안에서 훅스를 불러와야하기 때문에 모든 const가 App안에 들어와있는것.
+  const [account, setAccount] = useState("");
   const [myBalance, setMyBalance] = useState();
 
   const onClickAccount = async () => {
@@ -14,31 +20,48 @@ function App() {
       const accounts = await window.ethereum.request({
         method: "eth_requestAccounts",
       });
+
       setAccount(accounts[0]);
     } catch (error) {
       console.error(error);
     }
   };
-
   const onClickLogOut = () => {
     setAccount("");
   };
-
   const onClickBalance = async () => {
     try {
       if (!account || !contract) return;
 
       const balance = await contract.methods.balanceOf(account).call();
-      // const totalSupply = await contract.methods.totalSupply().call();
 
       setMyBalance(web3.utils.fromWei(balance));
     } catch (error) {
       console.error(error);
     }
   };
+  const onClickMint = async () => {
+    try {
+      if (!account) return;
+
+      const result = await nftContract.methods
+        .mintNft(
+          "https://gateway.pinata.cloud/ipfs/QmRB5xRpCke6UggULyDmXbd829j9YC24hcsyQVhBSkzNbY"
+        )
+        .send({ from: account });
+
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    console.log(nftContract);
+  }, []);
 
   return (
-    <div className="bg-green-100 min-h-screen flex justify-center items-center">
+    <div className="bg-red-100 min-h-screen flex justify-center items-center">
       {account ? (
         <div>
           <div className="text-main font-semibold text-2xl">
@@ -48,10 +71,15 @@ function App() {
               로그아웃
             </button>
           </div>
-          <div className="flex items-center">
+          <div className="flex items-center mt-4">
             {myBalance && <div>{myBalance} tMatic</div>}
             <button className="ml-2 btn-style" onClick={onClickBalance}>
               잔액 조회
+            </button>
+          </div>
+          <div className="flex items-center mt-4">
+            <button className="ml-2 btn-style" onClick={onClickMint}>
+              민팅
             </button>
           </div>
         </div>
